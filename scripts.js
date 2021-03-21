3131f77749a141495f32f85d5206780e97b7b324
Holly Jasiura <hollyj@Im-a-Computer.local>
2021-03-20 20:48:26 -0400

    modal working

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-console.log("connected");
+const modalOverlay = document.querySelector(".modalOverlay");
+const closeModelBtn = document.querySelector(".closeModelBtn");
+const body = document.querySelector("body");
+
+//show modal after a short delay
+const showModal = window.setTimeout(() => {
+  modalOverlay.classList.add("modalVisible");
+}, 0.2);
+
+//close modal when 'x' button clicked
+closeModelBtn.addEventListener("click", function () {
+  modalOverlay.classList.remove("modalVisible");
+});
+//close modal if user clicks outside of the modal when it's open
+body.addEventListener("click", function () {
+  console.log("clicked outside the modal");
+});
